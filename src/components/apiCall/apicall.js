9fdf0bc342d7d8a9b4b1c15739f18ee3264f1672
Vetrivelan <vetri0907.vv@gmail.com>
2021-09-23 23:43:59 +0530

    clean

--- src/components/apiCall/apicall.js
+++ src/components/apiCall/apicall.js
@@ -5,27 +5,7 @@ const api_url = "https://demooapi.herokuapp.com";
 
 // verify user email
 export const verifyuser = async (userName,email) => {
-    try {
-        await axios ({
-        method: 'POST',
-        url: `${api_url}/auth/verify`,
-        data: {
-        userName: userName,
-        email: email
-        },
-        headers: new Headers({
-            'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin': '*'
-        })
-        }).then(res => {
-            console.log(res);
-        })
-     }
-     catch(err) {
-          console.log(err);
-     }
-    // await axios.post(`${api_url}/auth/verify`,{userName,email})
-    // headers: new Headers({ 'Content-Type': 'application/json' })
-    // res.setHeader('Access-Control-Allow-Origin', '*')
-    // .then(result=>console.log(result))
+    await axios.post(`${api_url}/auth/verify`,{userName,email})
+    .then(result=>console.log(result))
+    .catch(error=>console.log(error))
 }
